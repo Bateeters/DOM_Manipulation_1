8b792cf793169630b0decc628994fe255efa9a98
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-02 06:35:31 -0400

    Added inline styling with JavaScript

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
 
+// inline styling
+para.style.color="white";
+para.style.backgroundColor="black";
+para.style.padding="10px";
+para.style.width="250px";
+para.style.textAlign='center';
+
+
+
 sect.appendChild(para);
 
 const text = document.createTextNode(" - the premier source for web development knowledge.");
